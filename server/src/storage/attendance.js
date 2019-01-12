79bc146b36b45a1f770d7d8d3b6dc40d7d966f81
Tiim <tim.bachmann96@gmail.com>
2019-01-12 11:13:39 +0100

    Add event endpoint and fix attendance fetching

--- server/src/storage/attendance.js
+++ server/src/storage/attendance.js
@@ -2,7 +2,7 @@ const { knex } = require('../db');
 
 module.exports = {
   async insert(eventId, personId, state) {
-    const ret = await knex('event')
+    const ret = await knex('attendance')
       .insert({ eventId, personId, state })
       .returning('id');
     return ret[0];
