4100cfbb91a7655d59c0b0cc582fabe4118a78b3
Tiim <tim.bachmann96@gmail.com>
2019-01-12 11:08:43 +0100

    Adding debug output to seeding command

--- server/cmd/seed.js
+++ server/cmd/seed.js
@@ -6,17 +6,22 @@ const { attendance, event, person, team } = require('../src/storage');
 knexCleaner
   .clean(knex)
   .then(async () => {
+    console.log('inserting teams');
     const team1 = await team.insert('Hyenas');
     const team2 = await team.insert('Baboons');
 
+    console.log('inserting persons');
     const person1 = await person.insert('Brenden Wilks', team1);
     const person2 = await person.insert('Irv Nardell', team1);
     await person.insert('Ashli Welpy', team2);
     await person.insert('Maxine Leung', team2);
 
+    console.log('inserting events');
     const event1 = await event.insert(new Date(2019, 01, 1), team1);
     const event2 = await event.insert(new Date(2019, 01, 2), team1);
+    const event3 = await event.insert(new Date(2019, 02, 2), team2);
 
+    console.log('inserting attendance');
     await attendance.insert(event1, person1, 'present');
     await attendance.insert(event2, person1, 'present');
     await attendance.insert(event1, person2, 'present');
