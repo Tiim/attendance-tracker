aa677e860da4397d1f2dc1bf33abbc5316a970db
Tiim <tim.bachmann96@gmail.com>
2019-01-27 21:17:54 +0100

    Add more tests to server

--- server/test/common.js
+++ server/test/common.js
@@ -3,7 +3,27 @@ const chai = require('chai');
 const server = require('../src/server').fastify;
 const should = chai.should();
 
+const http = {
+  get: async (url) => {
+    const res = await server.inject({ method: 'GET', url });
+    return { ...res, body: JSON.parse(res.payload) };
+  },
+  put: async (url, payload) => {
+    const res = await server.inject({ method: 'PUT', url, payload });
+    return { ...res, body: JSON.parse(res.payload) };
+  },
+  post: async (url, payload) => {
+    const res = await server.inject({ method: 'POST', url, payload });
+    return { ...res, body: JSON.parse(res.payload) };
+  },
+  delete: async (url) => {
+    const res = await server.inject({ method: 'DELETE', url });
+    return res;
+  },
+};
+
 module.exports = {
+  http,
   should,
   server,
   chai,
--- server/test/team.js
+++ server/test/team.js
@@ -1,28 +1,51 @@
-const { server } = require('./common');
+const { server, http } = require('./common');
 
 describe('Team', async () => {
-  describe('/GET book', async () => {
-    it('should GET all the teams but none should exist', async () => {
-      const res = await server.inject({
-        method: 'GET',
-        url: '/api/teams/',
-      });
-      res.statusCode.should.equal(200);
-      const body = JSON.parse(res.payload);
-      body.should.have.length(0);
+  it('should GET all the teams but none should exist', async () => {
+    const res = await http.get('/api/teams/');
+    res.statusCode.should.equal(200);
+    res.body.should.have.length(0);
+  });
+
+  it('should PUT some teams in', async () => {
+    const res = await http.put('/api/teams/', { name: 'Team 1' });
+
+    res.statusCode.should.equal(201);
+    res.body.name.should.equal('Team 1');
+  });
+
+  it('should PUT some teams in and GET them again', async () => {
+    const res = await http.put('/api/teams/', { name: 'TestTeam' });
+    const res2 = await http.get('/api/teams/');
+    res2.body.should.deep.include(res.body);
+  });
+
+  it('should DELETE team', async () => {
+    const res = await http.put('/api/teams/', { name: 'TestTeam' });
+    await http.delete(`/api/teams/${res.body.id}`);
+    const res2 = await http.get('/api/teams/');
+    res2.body.should.have.length(0);
+  });
+
+  it('should GET persons of team', async () => {
+    const team = await http.put('/api/teams', { name: 'Team1' });
+    const person = await http.put('/api/persons', {
+      name: 'Michael Jackson',
+      teamId: team.body.id,
     });
+    const res = await http.get(`/api/teams/${team.body.id}/persons`);
+    res.body.should.have.length(1);
+    res.body.should.deep.include(team.body);
+  });
 
-    it('should PUT some teams in', async () => {
-      const res = await server.inject({
-        method: 'PUT',
-        url: '/api/teams/',
-        payload: { name: 'Team 1' },
-      });
-
-      res.statusCode.should.equal(201);
-      const body = JSON.parse(res.payload);
-
-      body.name.should.equal('Team 1');
+  it('shoud GET events of team', async () => {
+    const team = await http.put('/api/teams', { name: 'Team1' });
+    const event = await http.put('/api/events', {
+      date: new Date(),
+      teamId: team.body.id,
     });
+    const res = await http.get(`/api/teams/${team.body.id}/events`);
+    res.body.should.have.length(1);
+    res.body.should.deep.include(team.body);
   });
 });
