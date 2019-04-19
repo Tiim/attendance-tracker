f1225805d9bfc4c4177cf2339b9de865d07b5d35
Tiim <tim.bachmann96@gmail.com>
2019-04-19 16:38:11 +0200

    Add race-result route.

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -6,9 +6,11 @@ const subjects = [
   { prefix: '/events', f: require('./event'), auth: true },
   { prefix: '/persons', f: require('./person'), auth: true },
   { prefix: '/teams', f: require('./team'), auth: true },
+  { prefix: '/race-result', f: require('./race-result'), auth: true },
 ];
 
 module.exports = function(fastify, opts, next) {
+  //authenticated routes
   const authenticated = subjects.filter((s) => s.auth === true);
   fastify.register((fastify, opts, next) => {
     fastify.addHook('preHandler', authMiddleware);
@@ -16,11 +18,21 @@ module.exports = function(fastify, opts, next) {
     next();
   });
 
+  //unauthenticated routes
   const open = subjects.filter((s) => s.auth === false);
   fastify.register((fastify, opts, next) => {
     open.forEach((s) => fastify.register(s.f, { prefix: s.prefix }));
     next();
   });
+
+  //invalid routes
+  subjects
+    .filter((s) => ![true, false].includes(s.auth))
+    .forEach((s) =>
+      console.log(
+        `WARNING: ${s.prefix} ignored: auth set incorrectly (${s.auth})`
+      )
+    );
 
   fastify.get('/', async (req, reply) => {
     const prefixes = subjects.map((s) => s.prefix);
